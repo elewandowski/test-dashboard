44fb2c52fa8d25118b4beb44004f99bb93e4747e
Emil Lewandowski <emil.ar.lew@gmail.com>
2022-08-28 19:30:39 +0200

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,19 @@
 module.exports = {
-    extends: ['eslint:recommended'],
-    parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 8,
-        ecmaFeatures: {
-            jsx: true,
-        },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    sourceType: 'script',
+    ecmaVersion: 8,
+    ecmaFeatures: {
+      jsx: true,
     },
-    rules: {
-        'react/jsx-uses-react': 'error',
-        'react/jsx-uses-vars': 'error',
-        'no-var': 2,
-    },
-    env: {
-        es6: true,
-        browser: true,
-        // amd: true,
-        node: true,
-    },
+  },
+  rules: {
+    'no-var': 2,
+  },
+  env: {
+    es6: true,
+    browser: true,
+    // amd: true,
+    node: true,
+  },
 }
