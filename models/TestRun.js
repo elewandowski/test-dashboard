6db839cc7f5f2dab74a30893b85be13d48a2e493
Emil Lewandowski <emil.ar.lew@gmail.com>
2022-08-30 21:39:28 +0200

    update TestRun model

--- models/TestRun.js
+++ models/TestRun.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const testRun = new mongoose.Schema({
-  name: { type: String },
+  test: { type: Schema.Types.ObjectId, ref: 'Test' },
   numberOfRetries: { type: Number },
-  suiteName: { type: String },
-  suiteFilePath: { type: String },
-  suiteTimeStamp: { type: Date },
+  // suiteName: { type: String },
+  // suiteFilePath: { type: String },
+  suiteRunTimeStamp: { type: Date },
+  passed: { type: Boolean },
+  failureMessage: { type: String },
   cypressRun: { type: Schema.Types.ObjectId, ref: 'CypressRun' },
 })
 
