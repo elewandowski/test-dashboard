0096e3f013f85fb9aa1c667c5cfab16b70c5449c
Emil Lewandowski <emil.ar.lew@gmail.com>
2022-08-31 23:35:40 +0200

    update view

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react'
-
-import logo from './logo.svg'
 import './App.css'
 import Table from './components/Table.js'
 
@@ -16,10 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Test Dashboard</h1>
 
         <Table data={response}></Table>
       </header>
