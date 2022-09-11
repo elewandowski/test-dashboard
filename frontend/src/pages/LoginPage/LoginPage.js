51bef39791797be5c143f7dd8845aacbbbdda42c
Emil Lewandowski <emil.ar.lew@gmail.com>
2022-09-11 13:55:14 +0200

    fix eslint warnings

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react'
+import { React, useState } from 'react'
 import PageShell from '../../components/PageShell/PageShell'
 import { Navigate } from 'react-router-dom'
 import UserContext from '../../contexts/UserContext'
--- frontend/src/pages/PageNotFoundPage/PageNotFoundPage.js
+++ frontend/src/pages/PageNotFoundPage/PageNotFoundPage.js
@@ -1,36 +1,7 @@
-import { React, useState } from 'react'
+import { React } from 'react'
 import PageShell from '../../components/PageShell/PageShell'
-import { Navigate, useSearchParams } from 'react-router-dom'
-import UserContext from '../../contexts/UserContext'
-import axios from 'axios'
 
 function PageNotFoundPage(props) {
-  const [searchParams] = useSearchParams()
-  const [signupFailed, setSignupFailed] = useState(false)
-  const [signedUp, setSignedUp] = useState(false)
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
-
-  function submitHandler(e, setUser) {
-    e.preventDefault()
-
-    const signupToken = searchParams.get('token')
-
-    axios
-      .post('/signup', { email, password, signupToken })
-      .then(async (res) => {
-        if (res.status === 200) {
-          localStorage.setItem('authToken', res.data?.authToken)
-
-          setUser(res.data.user)
-          axios.defaults.headers.common['Authorization'] = res.data?.authToken
-          setSignedUp(true)
-        } else {
-          setSignupFailed(true)
-        }
-      })
-  }
-
   return (
     <div className="PageNotFoundPage">
       <PageShell>
