4e8db23d9dcde3033b4c77ca2f5ee76558c26943
Emil Lewandowski <emil.ar.lew@gmail.com>
2023-03-06 19:58:04 +0100

    better signup error handling

--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -6,24 +6,35 @@ const User = require('../models/User')
 
 router
   .post('/', async function (req, res) {
+    if (!req.body.signupToken) {
+      return res.status(400).send('Signup token missing')
+    }
+
     const signupTokenDB = await SignupToken.findOne({
       token: req.body.signupToken,
     })
-    if (signupTokenDB && signupTokenDB.isValid()) {
-      const user = await User.create({
-        email: req.body.email,
-        password: req.body.password,
-      })
-      signupTokenDB.invalidate()
 
-      const authToken = await AuthToken.create({
-        user: user._id,
-      })
-
-      res.send({ authToken: authToken.authToken, user })
-    } else {
-      res.sendStatus(401)
+    if (!signupTokenDB || !signupTokenDB.isValid()) {
+      return res.status(400).send('Signup token invalid')
     }
+
+    const emailConflict = await User.findOne({ email: req.body.email })
+
+    if (emailConflict) {
+      return res.status(400).send('User with email already exists')
+    }
+
+    const user = await User.create({
+      email: req.body.email,
+      password: req.body.password,
+    })
+    signupTokenDB.invalidate()
+
+    const authToken = await AuthToken.create({
+      user: user._id,
+    })
+
+    res.send({ authToken: authToken.authToken, user })
   })
   .get('/gettoken', async function (req, res) {
     const signupToken = await SignupToken.create({})
